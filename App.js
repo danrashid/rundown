21bef7d096aafd42515b4483b5476286d3a5d74a
Dan Rashid <dan@pecsafety.com>
2020-02-12 10:55:55 -0600

    Follow https://docs.expo.io/versions/v36.0.0/sdk/task-manager/#example

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
+import * as TaskManager from "expo-task-manager";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const LOCATION_TASK_NAME = "background-location-task";
+
+export default class Component extends React.Component {
+  onPress = async () => {
+    const { status } = await Location.requestPermissionsAsync();
+    if (status === "granted") {
+      await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+        accuracy: Location.Accuracy.Balanced
+      });
+    }
+  };
+
+  render() {
+    return (
+      <TouchableOpacity onPress={this.onPress}>
+        <Text>Enable background location</Text>
+      </TouchableOpacity>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (data) {
+    const { locations } = data;
+    console.log(locations);
   }
 });
